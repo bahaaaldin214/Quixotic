54d435e5da09405ef02b86ec13f2950b75003643
bahaaaldin214 <bahaaaldinmohammad@gmail.com>
2020-04-27 04:02:09 -0500

    added examples

---
+++ examples/main.js
@@ -0,0 +1,19 @@
+import QuixoticEngine from "/quixotic.min.js";
+import game from "./example1/index.js";
+
+window.onload = () => {
+  
+  const engine = QuixoticEngine.create(game.create)
+    .then(engine => {
+    
+    game.start(engine);
+    
+    engine.onSpeedChange = (value) => {
+    
+      console.log("Game current speed is " + value);
+    
+    };
+    
+  }).catch(console.error);
+  
+};
