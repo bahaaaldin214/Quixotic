50a744f0ca8ec8ba26b5bf62f3dacb575831c0fc
Bahaa <bahaaaldinmohammad@gmail.com>
2020-06-03 12:52:36 -0500

    Lots of bug fixes, adding new feature to create a background buffer

--- src/collisionHandler.js
+++ src/collisionHandler.js
@@ -1,151 +1,95 @@
-export default class CollisionHandler{
-  
-  constructor(world){
-    
-    this.world = world;
-    
+import engineMath from "./modules/engineMath.js";
+
+export default class Controller{
+
+  constructor(){
+    this.keysPressed = {};
+    this.functionalKeys = {};
+    this.functionalKeysUp = {};
+    this.mouseButtons = [];
+    this.mouseDown = {x: 0, y: 0, down: false};
+
+    this.keyDownUp = this.keyDownUp.bind(this);
   }
-  
-  checkCollide(object){
-    
-    const {world, colliding} = this;
-    
-    if(!world.objectsCollisionInfo[object.name]){
-      return;
+
+  keyDownUp(e){
+
+    const key = e.key,
+    {functionalKeysUp, keysPressed, functionalKeys} = this;
+
+    if(e.type == "keyup"){
+
+      if(functionalKeysUp[key]){
+        e.preventDefault();
+        functionalKeysUp[key]();
+      }
+      keysPressed[key] = false;
+    } else {
+      keysPressed[key] = true;
+      if(functionalKeys[key]) e.preventDefault();
     }
-    
-    const info = world.objectsCollisionInfo[object.name];
-    
-    const allObjectsObject = world.objects;
-    const allObjects = Object.keys(info).map(key => {
-      
-      if(typeof allObjectsObject[key] == "undefined"){
-        
-        throw new TypeError("Trying to get collision Info Of an undefined object.");
-        
-      }
-      return allObjectsObject[key];
-    });
 
-
-    for(let i = allObjects.length; i--;){
-      
-      const currentObjectsList = allObjects[i];
-      
-      if(!currentObjectsList){
-
-        continue;
-      }
-      
-      if(!Array.isArray(currentObjectsList)){
-        this.checkCollideObjects(object, currentObjectsList, info[currentObjectsList.name]);
-        
-        continue;
-      }
-      
-      for(let j = currentObjectsList.length; j--;){
-        
-        const currentObject = currentObjectsList[j];
-        const name = currentObject.name;
-        
-        if(currentObject.delete){
-
-          allObjectsObject[name].splice(j, 1);
-          continue;
-        }
-        
-        if(currentObject == object){
-          continue;
-        }
-        
-        this.checkCollideObjects(object, currentObject, info[name]);
-      }
-      
-    }
   }
-  
- checkCollideObjects(object1, object2, info) {
+  mouseDownUpMove(x, y, e){
 
-    const angle = this.colliding(object1, object2);
-    
-    if(object1.checkedCollide.has(object2) || object2.checkedCollide.has(object1)){
-      return;
-    }
+    if(e.type == "mousemove") { this.mouseDown.x = x; this.mouseDown.y = y; return }
+    this.mouseDown = {x: x, y: y, down: e.type == "mousedown"};
+    return this;
+  }
 
-    object1.checkedCollide.set(object2, true);
+  attachMouseButton(x, y, w, h, behavior){
+    this.mouseButtons.push({x: x, y: y, w: w, h: h, call: behavior});
+  }
 
-    if (angle) {
+  detachMouseButton(i){
+    console.log("fix maybe?");
+    this.mouseButtons.splice(i);
+  }
 
-      if (info.call) {
-        object1[info.call](object2);
+  attachKeyDown(key, call){
+    this.functionalKeys[key] =  call;
+    return this;
+  }
 
+  attachKeyUp(key, call){
+    this.functionalKeysUp[key] =  call;
+    return this;
+  }
+
+  detachKeyUp(key){
+     delete this.functionalKeysUp[key];
+  }
+
+  detachKeyDown(key){
+     delete this.functionalKeys[key];
+  }
+
+  update(){
+
+    const {mouseDown, mouseButtons, keysPressed, functionalKeys} = this;
+    const functionalKeysKeys = Object.keys(functionalKeys);
+
+    for(let i = functionalKeysKeys.length;i--;){
+      let key = functionalKeysKeys[i];
+
+      if(keysPressed[key]){
+        functionalKeys[key]();
       }
 
-      if (info.elastic) {
+    }
+    if(mouseDown.down){
 
-        const velocity = object1.velocity;
-        const [vx1, vy1] = object1.velocity;
+      for(let i = mouseButtons.length;i--;){
+        let {x, y} = mouseDown;
 
-        const _velocity = object2.velocity;
-        const [vx2, vy2] = _velocity;
+        let button = mouseButtons[i];
 
-        const PI = Math.PI;
-
-        if ((angle >= 0 && angle < PI / 4) || (angle > 7 / 4 * PI && angle < PI * 2)) {
-          if (vx1 > 0) velocity[0] = -vx1;
-          if (vx2 < 0) _velocity[0] = -vx2;
-
-        } else if (angle >= PI / 4 && angle < 3 / 4 * PI) {
-          if (vy1 > 0) velocity[1] = -vy1;
-          if (vy2 < 0) _velocity[1] = -vy2;
-
-        } else if (angle >= 3 / 4 * PI && angle < 5 / 4 * PI) {
-          if (vx1 < 0) velocity[0] = -vx1;
-          if (vx2 > 0) _velocity[0] = -vx2;
-          
-        } else {
-          if (vy1 < 0) velocity[1] = -vy1;
-          if (vy2 > 0) _velocity[1] = -vy2;
+        if(engineMath.between(x, button.x, button.w) && engineMath.between(y, button.y, button.h)){
+          button.call(x, y);
         }
       }
     }
-  }
-  
-  colliding(o1, o2){
-    
-    const [x1, y1] = o1.coords;
-    const [w1, h1] = o1.area;
-    
-    const halfWidth1 = w1/2;
-    const halfHeight1 = h1/2;
-    
-    const left1 = x1 - halfWidth1;
-    const right1 = x1 + halfWidth1;
-    const top1 = y1 - halfHeight1;
-    const bottom1 = y1 + halfHeight1;
-    
-    const [x2, y2] = o2.coords;
-    const [w2, h2] = o2.area;
-      
-    const halfWidth2 = w2/2;
-    const halfHeight2 = h2/2;
-      
-    const left2 = x2 - halfWidth2;
-    const right2 = x2 + halfWidth2;
-    const top2 = y2 - halfHeight2;
-    const bottom2 = y2 + halfHeight2;
-    
-    if(left1 <= right2 && right1 >= left2 && top1 <= bottom2 && bottom1 >= top2){
-      
-      let dx = x2 - x1;
-      let dy = y2 - y1;
 
-      let angle = Math.atan2(dy, dx);
-      if (angle < 0) angle += Math.PI*2;
-      
-      return angle;
-
-    }
   }
-  
 }
+
